5ed4e52bab9c07edceab6374614421bfc29fe76e
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-06-27 16:56:59 +0530

    background change feature added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Nav from './Nav';
 import Home from './Home';
@@ -9,20 +9,44 @@ import Project from './Project';
 
 
 
-function App() {
-  return (
-    <Router>
-       <div className="app">
-        <Nav/>
-        <Route path="/" exact component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/projects" component = {Project}/>
-        <Footer/>
-      </div>
-    </Router>
+
+
+
+class App extends Component {
+
+  constructor(){
+      super()
+      this.state={
+        color:true
+      }
+    }
+
+  render(){
     
     
-  );
+
+    this.colorChange=()=>{
+      this.setState({
+        color:!this.state.color
+      })
+      
+    }
+    
+    return(
+        <Router>
+          <div className="app" onClick={this.colorChange}  style={{
+            "backgroundColor": this.state.color ? '#1D0303' : '#070F2C'
+          }}>
+          <Nav/>
+          <Route path="/" exact component={Home} onClick={this.colorChange}/>
+          <Route path="/about" component={About}/>
+          <Route path="/projects" component = {Project}/>
+          <Footer/>
+          </div>
+        </Router>
+    )
+  }
 }
+ 
 
 export default App;
